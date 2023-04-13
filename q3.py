e1c6bd31bdd89cbdb74a00694f2ae11f12fe6276
Snehasish <snehasishnath88@gmail.com>
2023-04-13 10:57:59 +0530

    q3

--- q3.py
+++ q3.py
@@ -10,3 +10,15 @@ class Solution:
 
             seen[num]=i
         return []
+
+
+
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                sum=nums[i]+nums[j]
+                if sum==target:
+                    return [i,j]
+"""
